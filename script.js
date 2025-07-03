784bc78bb0e7b5c35cef52ea9f3b37d38f87d690
Gleice01 <gleiceleeandra2013@gmail.com>
2025-07-03 01:34:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const btn = document.getElementById("actionBtn");
 const msg = document.getElementById("message");
 
 btn.addEventListener("click", () => {
-    msg.textContent =
-        "Quando você não pensa em desistir, o sucesso é inevitável.";});
+    msg.textContent = "Quando você não pensa em desistir, o sucesso é inevitável.";
+});
